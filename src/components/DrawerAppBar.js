1f1bc5fee5d9f72f1463137928f65b9a0b30ee4b
Happy Tom <mithunaksh01@gmail.com>
2024-10-17 00:02:49 +0530

    added dark theme

--- src/components/DrawerAppBar.js
+++ src/components/DrawerAppBar.js
@@ -1,21 +1,30 @@
-import * as React from "react";
+import React from "react";
 import PropTypes from "prop-types";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import CssBaseline from "@mui/material/CssBaseline";
-import Divider from "@mui/material/Divider";
-import Drawer from "@mui/material/Drawer";
-import IconButton from "@mui/material/IconButton";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemText from "@mui/material/ListItemText";
-import MenuIcon from "@mui/icons-material/Menu";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
+import {
+  AppBar,
+  Box,
+  CssBaseline,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Toolbar,
+  Typography,
+  Button,
+  ThemeProvider,
+  createTheme,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu"; // Correct import for MenuIcon
+import DarkModeIcon from "@mui/icons-material/DarkMode"; // Import icon for dark mode
+import LightModeIcon from "@mui/icons-material/LightMode"; // Import icon for light mode
 
-import JourneyList from "./JourneyList";
+// Import components
+import JourneyList from "./JourneyList"; // Journeys component
+import Dashboard from "./Dashboard"; // Dashboard component
+import Master from "./Master"; // Master component
 
 const drawerWidth = 240;
 const navItems = ["Home", "Journeys", "Master"];
@@ -23,9 +32,21 @@ const navItems = ["Home", "Journeys", "Master"];
 function DrawerAppBar(props) {
   const { window } = props;
   const [mobileOpen, setMobileOpen] = React.useState(false);
+  const [activeComponent, setActiveComponent] = React.useState("Home");
+  const [darkMode, setDarkMode] = React.useState(false); // State for dark mode
 
   const handleDrawerToggle = () => {
     setMobileOpen((prevState) => !prevState);
+  };
+
+  const handleNavClick = (item) => {
+    setActiveComponent(item);
+    setMobileOpen(false);
+  };
+
+  // Handle theme toggle
+  const handleThemeChange = () => {
+    setDarkMode((prevMode) => !prevMode);
   };
 
   const drawer = (
@@ -37,7 +58,10 @@ function DrawerAppBar(props) {
       <List>
         {navItems.map((item) => (
           <ListItem key={item} disablePadding>
-            <ListItemButton sx={{ textAlign: "center" }}>
+            <ListItemButton
+              sx={{ textAlign: "center" }}
+              onClick={() => handleNavClick(item)}
+            >
               <ListItemText primary={item} />
             </ListItemButton>
           </ListItem>
@@ -49,61 +73,96 @@ function DrawerAppBar(props) {
   const container =
     window !== undefined ? () => window().document.body : undefined;
 
+  const theme = createTheme({
+    palette: {
+      mode: darkMode ? "dark" : "light", // Use state for theme mode
+    },
+  });
+
+  const renderComponent = () => {
+    switch (activeComponent) {
+      case "Home":
+        return <Dashboard />;
+      case "Journeys":
+        return <JourneyList />;
+      case "Master":
+        return <Master />;
+      default:
+        return <Dashboard />;
+    }
+  };
+
   return (
-    <Box sx={{ display: "flex" }}>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
-      <AppBar component="nav">
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: "none" } }}
+      <Box sx={{ display: "flex" }}>
+        <AppBar component="nav">
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              sx={{ mr: 2, display: { sm: "none" } }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+            >
+              Journey Manager
+            </Typography>
+            <Box sx={{ display: { xs: "none", sm: "block" } }}>
+              {navItems.map((item) => (
+                <Button
+                  key={item}
+                  sx={{ color: "#fff" }}
+                  onClick={() => handleNavClick(item)}
+                >
+                  {item}
+                </Button>
+              ))}
+            </Box>
+            <IconButton
+              color="inherit"
+              onClick={handleThemeChange} // Toggle theme on click
+              aria-label="toggle dark mode"
+            >
+              {darkMode ? <LightModeIcon /> : <DarkModeIcon />}{" "}
+              {/* Change icon based on mode */}
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+
+        <nav>
+          <Drawer
+            container={container}
+            variant="temporary"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            ModalProps={{
+              keepMounted: true,
+            }}
+            sx={{
+              display: { xs: "block", sm: "none" },
+              "& .MuiDrawer-paper": {
+                boxSizing: "border-box",
+                width: drawerWidth,
+              },
+            }}
           >
-            <MenuIcon />
-          </IconButton>
-          <Typography
-            variant="h6"
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
-          >
-            Journey Manager
-          </Typography>
-          <Box sx={{ display: { xs: "none", sm: "block" } }}>
-            {navItems.map((item) => (
-              <Button key={item} sx={{ color: "#fff" }}>
-                {item}
-              </Button>
-            ))}
-          </Box>
-        </Toolbar>
-      </AppBar>
-      <nav>
-        <Drawer
-          container={container}
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-          sx={{
-            display: { xs: "block", sm: "none" },
-            "& .MuiDrawer-paper": {
-              boxSizing: "border-box",
-              width: drawerWidth,
-            },
-          }}
-        >
-          {drawer}
-        </Drawer>
-      </nav>
-      <Box component="main" sx={{ p: 3 }}>
-        <Toolbar />
-        <JourneyList /> {/* Journey List Component */}
+            {drawer}
+          </Drawer>
+        </nav>
+
+        <Box component="main" sx={{ p: 3, width: "100%" }}>
+          <Toolbar />
+          {renderComponent()} {/* Render the selected component */}
+        </Box>
       </Box>
-    </Box>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/Master.js
@@ -0,0 +1,13 @@
+// Master.js
+import React from "react";
+
+function Master() {
+  return (
+    <div>
+      <h1>Master</h1>
+      <p>This is the Master page.</p>
+    </div>
+  );
+}
+
+export default Master;
